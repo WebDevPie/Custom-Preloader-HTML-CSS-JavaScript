69abc46331d60176fbbd967e72ded6fa704f2839
WebDevPie <nazarmarchyshyn@gmail.com>
2022-11-24 20:49:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {   
+    setTimeout(()=>{
+        $(".loader__frame").fadeIn();
+    }, 1000);
+
+    setTimeout(()=>{
+        $(".loader__square").fadeIn();
+    }, 3000);
+});
